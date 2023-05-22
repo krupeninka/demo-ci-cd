ed1217c3c2d0f3354e75ba05d9f868aaa1579758
Konstantin Krupenin <krupeninka@gmail.com>
2023-05-22 10:35:38 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleButtonClick = () => {
+    setCount(count + 1); /* Внимание! строка содержащая ошибки */
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Счетчик:
+        {count}
+      </h1>
+      <button type="button" onClick={handleButtonClick}>Увеличить счетчик</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('increases count when button is clicked', () => {
+  const { getByText } = render(<App />);
+  const button = getByText(/Увеличить счетчик/i);
+  const count = getByText(/Счетчик:/i);
+
+  expect(count).toHaveTextContent('0');
+  fireEvent.click(button);
+  expect(count).toHaveTextContent('1');
 });
